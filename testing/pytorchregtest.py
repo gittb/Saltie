f7202f962ecd1d0d226c0e3e055f693335b7d784
gittb <gitterbd@gmail.com>
2018-12-26 16:40:07 -0600

    pytorch regression testing

--- testing/pytorchregtest.py
+++ testing/pytorchregtest.py
@@ -8,9 +8,38 @@ y = pickle.load(open('y.dset', 'rb'))
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import torch.utils.data as Data
 
 
 numepochs = 5
+
+
+torch.manual_seed(1)    # reproducible
+
+BATCH_SIZE = 512
+# BATCH_SIZE = 8
+
+x = torch.tensor(x)      # this is x data (torch tensor)
+y = torch.tensor(y)  # this is y data (torch tensor)
+
+torch_dataset = Data.TensorDataset(x, y)
+loader = Data.DataLoader(
+    dataset=torch_dataset,      # torch TensorDataset format
+    batch_size=BATCH_SIZE,      # mini batch size
+    shuffle=True,               # random shuffle for training
+    num_workers=3,              # subprocesses for loading data
+    pin_memory=True
+)
+
+
+def show_batch():
+    for epoch in range(3):   # train entire dataset 3 times
+        for step, (batch_x, batch_y) in enumerate(loader):  # for each training step
+            # train your data...
+            print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+                  batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
+
 
 
 def unison_shuffled_copies(a, b):
@@ -31,6 +60,7 @@ class Net(torch.nn.Module):
         return x
 
 net = Net(n_feature=177, n_hidden=600, n_output=105)     # define the network
+net.cuda()
 print(net)  # net architecture
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
@@ -41,27 +71,30 @@ stamp = []
 plt.ion()   # something about plotting
 
 
-for epoch in range(numepochs):
-    x, y = unison_shuffled_copies(x, y)
+if __name__ == '__main__':
+    for epoch in range(numepochs):
+
+        for step, (batch_x, batch_y) in enumerate(loader):
+            b_x = batch_x.cuda()
+            b_y = batch_y.cuda()
+
+            prediction = net(b_x)     # input x and predict based on x
+
+            loss = loss_func(prediction, b_y)     # must be (1. nn output, 2. target)
+
+            optimizer.zero_grad()   # clear gradients for next train
+            loss.backward()         # backpropagation, compute gradients
+            optimizer.step()        # apply gradients
+
+            if step % 50 == 0:
+                print(step, loss)
+                losses.append(loss)
+                stamp.append(((epoch + 1) * x.shape[0]) + step)
 
 
-    for row in range(x.shape[0]):
-        prediction = net(x[row])     # input x and predict based on x
-
-        loss = loss_func(prediction, y[row])     # must be (1. nn output, 2. target)
-
-        optimizer.zero_grad()   # clear gradients for next train
-        loss.backward()         # backpropagation, compute gradients
-        optimizer.step()        # apply gradients
-
-        if row % 50 == 0:
-            losses.append(loss)
-            stamp.append(((epoch + 1) * x.shape[0]) + row)
-
-
-plt.cla()
-plt.plot(loss, stamp, 'r-', lw=5)
-plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
-plt.pause(0.1)
-plt.ioff()
-plt.show()
+    plt.cla()
+    plt.plot(loss.numpy(), stamp, 'r-', lw=5)
+    plt.text(0.5, 0, 'Loss=%.4f' % loss.data.numpy(), fontdict={'size': 20, 'color':  'red'})
+    plt.pause(0.1)
+    plt.ioff()
+    plt.show()
