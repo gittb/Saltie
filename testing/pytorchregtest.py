3358b4869ff362a11ec2776bbdaf46dccd7756ab
gittb <gitterbd@gmail.com>
2018-12-29 14:20:02 -0600

    pytorch regression testing

--- testing/pytorchregtest.py
+++ testing/pytorchregtest.py
@@ -16,8 +16,7 @@ numepochs = 15
 
 torch.manual_seed(1)    # reproducible
 
-BATCH_SIZE = 128
-# BATCH_SIZE = 8
+BATCH_SIZE = 256
 
 x = torch.tensor(x)     # this is x data (torch tensor)
 y = torch.tensor(y)  # this is y data (torch tensor)
@@ -49,21 +48,25 @@ def unison_shuffled_copies(a, b):
 
 
 class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden, n_output):
+    def __init__(self, n_feature, n_hidden, n_hidden2, n_hidden3, n_output):
         super(Net, self).__init__()
         self.hidden = torch.nn.Linear(n_feature, n_hidden)   # hidden layer
-        self.predict = torch.nn.Linear(n_hidden, n_output)   # output layer
+        self.hidden2 = torch.nn.Linear(n_hidden, n_hidden2)
+        self.hidden3 = torch.nn.Linear(n_hidden2, n_hidden3)
+        self.predict = torch.nn.Linear(n_hidden3, n_output)   # output layer
 
     def forward(self, x):
-        x = F.tanh(self.hidden(x))      # activation function for hidden layer
+        x = F.tanh(self.hidden(x))    # activation function for hidden layer
+        x = F.tanh(self.hidden2(x))
+        x = F.tanh(self.hidden3(x))
         x = self.predict(x)             # linear output
         return x
 
-net = Net(n_feature=177, n_hidden=200, n_output=105)     # define the network
+net = Net(n_feature=177, n_hidden=3200, n_hidden2=1600, n_hidden3=800, n_output=105)     # define the network
 net.cuda()
 print(net)  # net architecture
 
-optimizer = torch.optim.RMSprop(net.parameters(), lr=0.001)
+optimizer = torch.optim.ASGD(net.parameters(), lr=0.002)
 loss_func = torch.nn.MSELoss()  # this is for regression mean squared loss
 
 losses = []
